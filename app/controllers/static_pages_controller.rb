c0a3481e24ce9c495a4cba2e4e4f18a7a72da191
Dat <trongdatdinh1@gmail.com>
2018-12-17 01:16:05 +0700

    manipulating micropost

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
+    if logged_in?
+      @micropost = current_user.microposts.build
+    end
   end
 
   def help
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
     post "/login", to: "sessions#create"
     delete "/logout", to: "sessions#destroy"
     resources :users
+    resources :microposts, only: [:create, :destroy]
   end
   resources :account_activations, only: [:edit]
   resources :password_resets, except: [:index, :show, :delete]
